87e929c0222aff501d78b8d3a6aa3e7f00d670f1
boronngo <boronngo@gmail.com>
2014-05-02 14:00:54 +0900

    setup.shの追加

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/home/boro/.dotfiles/.vim
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "シンボリックリンクを貼ります"
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.ssh/config ~/.ssh/config
